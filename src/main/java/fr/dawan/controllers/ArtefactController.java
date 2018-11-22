a27b875740c1fd8f02ea8487bf77604ea4c8fe4b
Anni <annemarkduan@gmail.com>
2018-11-22 09:23:04 +0100

    modif dodo nourriture jsp dao controller, servlet

--- src/main/java/fr/dawan/controllers/ArtefactController.java
+++ src/main/java/fr/dawan/controllers/ArtefactController.java
@@ -7,20 +7,49 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.dawan.DAO.InterfaceDao;
+import fr.dawan.beans.Dodo;
 import fr.dawan.beans.Jeux;
+import fr.dawan.beans.Nourriture;
 
 @Controller
 public class ArtefactController {
+
 	@Autowired
 	@Qualifier("hibernateDao3")
 	private InterfaceDao<Jeux> jeuDao;
+
+	@Autowired
+	@Qualifier("hibernateDao4")
+	private InterfaceDao<Dodo> dodoDao;
 	
+	
+	@Autowired
+	@Qualifier("hibernateDao5")
+	private InterfaceDao<Nourriture> nourritureDao;
+
+	
+
 	@PostMapping("/creajeux")
 	public String createGame(Jeux jeux, Model model) {
 		jeuDao.createOrUpdate(jeux);
 		model.addAttribute("msg", "Jouet crée!");
-		
-		
+
+		return "animaljoueur";
+	}
+
+	@PostMapping("/creadodo")
+	public String createSleep(Dodo dodo, Model model) {
+		dodoDao.createOrUpdate(dodo);
+		model.addAttribute("msg", "Dodo crée!");
+
+		return "animaljoueur";
+	}
+
+	@PostMapping("/creanourriture")
+	public String createFood(Nourriture nourriture, Model model) {
+		nourritureDao.createOrUpdate(nourriture);
+		model.addAttribute("msg", "Nourriture crée!");
+
 		return "animaljoueur";
 	}
 
