26b9f1b5a730716face4a626d4195e01d801c9d2
WayneAlphonse <marie.chiron44@gmail.com>
2018-11-22 10:56:46 +0100

    supression de petits trucs

--- src/main/java/fr/dawan/beans/Dodo.java
+++ src/main/java/fr/dawan/beans/Dodo.java
@@ -3,35 +3,28 @@ package fr.dawan.beans;
 import javax.persistence.Entity;
 
 @Entity
-public class Dodo extends DbObject{
-	private int tauxSommeil;
-	private int dureeSommeil;
+public class Dodo extends DbObject {
+	private int tauxSommeil = 0;
 	private String lieu;
-	
-	
-	
+
 	public Dodo() {
-	
+
 	}
+
 	public int getTauxSommeil() {
 		return tauxSommeil;
 	}
+
 	public void setTauxSommeil(int tauxSommeil) {
 		this.tauxSommeil = tauxSommeil;
 	}
-	public int getDureeSommeil() {
-		return dureeSommeil;
-	}
-	public void setDureeSommeil(int dureeSommeil) {
-		this.dureeSommeil = dureeSommeil;
-	}
+
 	public String getLieu() {
 		return lieu;
 	}
+
 	public void setLieu(String lieu) {
 		this.lieu = lieu;
 	}
-	
-	
 
 }
